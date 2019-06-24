0ebb0852d49127e6d09693da2550b2d3eb1ca61b
Martinj98 <martin.dejongh@student.hu.nl>
2019-06-24 17:10:16 +0200

    include logger

--- src/main/java/org/nl/hu/sie/bep/domain/adapter/LoadAdapterImpl.java
+++ src/main/java/org/nl/hu/sie/bep/domain/adapter/LoadAdapterImpl.java
@@ -6,10 +6,14 @@ import java.util.List;
 import org.nl.hu.sie.bep.loader.adapter.DatabaseAdapter;
 import org.nl.hu.sie.bep.loader.generic.Adapter;
 import org.nl.hu.sie.bep.loader.models.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class LoadAdapterImpl implements Adapter {
+	Logger logger = LoggerFactory.getLogger(LoadAdapterImpl.class);
 	DatabaseAdapter databaseadadapter= new DatabaseAdapter();
-	
+
 	@Override
 	public List<Data> returnDataFromMonth(int month) {
 		try {
@@ -17,6 +21,7 @@ public class LoadAdapterImpl implements Adapter {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.error("something went wrong trying to load the data", e);
 		}
 		return null;
 	}
